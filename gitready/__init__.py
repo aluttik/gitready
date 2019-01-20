44a49b0a7f9413696c722ae422340087644ea0d4
Arie van Luttikhuizen <aluttik@gmail.com>
2019-01-19 17:05:23 -0800

    Increased version to 0.0.1

--- gitready/__init__.py
+++ gitready/__init__.py
@@ -5,7 +5,7 @@ __title__ = "gitready"
 __summary__ = "Initializes a git repo for a python project"
 __url__ = "https://github.com/aluttik/gitready"
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 __author__ = "Arie van Luttikhuizen"
 __email__ = "aluttik@gmail.com"
